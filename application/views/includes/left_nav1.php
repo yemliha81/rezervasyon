80079dc8350f9458988649806b30ca4aae31b469
yemliha81 <yemlihaddln@hotmail.com>
2023-12-17 00:09:06 +0300

    fix

--- application/views/includes/left_nav1.php
+++ application/views/includes/left_nav1.php
@@ -7,7 +7,7 @@
         <span class="xflex"><span class="lnr lnr-calendar-full"></span> Rezervasyon</span> 
         <?php if($menu=='2'){echo '<i class="fa fa-chevron-right"></i>';}?>
     </a>
-    <a class="left-a <?php if($menu=='3'){echo 'active';}?>" href="<?php echo RESERVATION_CALENDAR;?>">
+    <a class="left-a <?php if($menu=='3'){echo 'active';}?>" href="javascript:;">
         <span class="xflex"><span class="lnr lnr-users"></span> Kullanıcı İşlemleri</span> 
         <?php if($menu=='3'){echo '<i class="fa fa-chevron-right"></i>';}?>
     </a>
